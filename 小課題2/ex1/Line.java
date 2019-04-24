8e2bc8a5999e639b898946fdbb9fb8a1dea86c48
s1250206 <49462476+s1250206@users.noreply.github.com>
2019-04-24 11:43:38 +0900

    Add files via upload

---
+++ 小課題2/ex1/Line.java
@@ -0,0 +1,19 @@
+package ex1;
+
+public class Line {
+	public Point p,q;
+
+	void setLine(Point p,Point q) {
+		this.p = p;
+		this.q = q;
+	}
+
+
+	Point getPointP() {
+		return this.p;
+	}
+
+	Point getPointQ() {
+		return this.q;
+	}
+}
